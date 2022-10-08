6313ad76b692462f2912500ceb1211a7050ab3bf
lihongbo <1581107617@qq.com>
2022-10-08 10:04:19 +0800

    hot-fix commit 2

--- src/main/java/com/lihongbo/git/GitTestJava.java
+++ src/main/java/com/lihongbo/git/GitTestJava.java
@@ -6,5 +6,6 @@ public class GitTestJava {
         System.out.println("hello Git2!!!!");
         System.out.println("hello Git3!!!!");
         System.out.println("hello Git4!!!!");
+        System.out.println("hot-fix test Git5!!!!");
     }
 }
