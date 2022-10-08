537d01fe220e98fcd8b692e2a47924def3c6461a
lihongbo <1581107617@qq.com>
2022-10-08 09:56:09 +0800

    second commit

--- src/main/java/com/lihongbo/git/GitTestJava.java
+++ src/main/java/com/lihongbo/git/GitTestJava.java
@@ -3,5 +3,6 @@ package com.lihongbo.git;
 public class GitTestJava {
     public static void main(String[] args) {
         System.out.println("hello Git!!!!");
+        System.out.println("hello Git2!!!!");
     }
 }
